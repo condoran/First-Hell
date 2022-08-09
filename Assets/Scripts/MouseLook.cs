71e5d87a6e10e3a8f08bd1c3f672e41a56f01fb6
Condor Andrei <44895249+condoran@users.noreply.github.com>
2022-08-09 20:23:42 +0300

    mouseLook

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
+    [SerializeField] Transform playerTransform;
+    [SerializeField] float mouseSensitivity = 50f;
+    
+    private float mouseX;
+    private float mouseY;
+    private float xRotation = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,13 @@ public class MouseLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // float mouseX = UnityEngine.InputSystem.Mouse.current.delta.ReadValue();
+        mouseX = UnityEngine.InputSystem.Mouse.current.delta.x.ReadValue() * mouseSensitivity * Time.deltaTime;
+        mouseY = UnityEngine.InputSystem.Mouse.current.delta.y.ReadValue() * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerTransform.Rotate(Vector3.up * mouseX);
     }
 }
